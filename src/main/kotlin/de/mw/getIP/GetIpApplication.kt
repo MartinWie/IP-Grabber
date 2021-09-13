5cc43f4952d8f4af287f0e6daaeb0e775b597662
MartinWie <martin.wiechmann.office@gmail.com>
2021-09-13 09:03:47 +0200

    initial commit with quickly hacked together solution / initial mvp

---
+++ src/main/kotlin/de/mw/getIP/GetIpApplication.kt
@@ -0,0 +1,11 @@
+package de.mw.getIP
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GetIpApplication
+
+fun main(args: Array<String>) {
+    runApplication<GetIpApplication>(*args)
+}
---
+++ src/test/kotlin/de/mw/getIP/GetIpApplicationTests.kt
@@ -0,0 +1,12 @@
+package de.mw.getIP
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GetIpApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
