3c2e827b56191db1937ba44967929d34db75d32c
MartinWie <martin.wiechmann.office@gmail.com>
2021-09-17 23:46:33 +0200

    added mapping for "/"

--- src/main/kotlin/de/mw/getIP/GetIpApplication.kt
+++ src/main/kotlin/de/mw/getIP/GetIpApplication.kt
@@ -3,6 +3,10 @@ package de.mw.getIP
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 @ComponentScan(basePackages = arrayOf("de.mw.getIP"))
@@ -10,4 +14,12 @@ class GetIpApplication
 
 fun main(args: Array<String>) {
     runApplication<GetIpApplication>(*args)
+}
+
+@RestController
+class Default {
+    @GetMapping("/")
+    @ResponseStatus(HttpStatus.OK)
+    fun default() {
+    }
 }
